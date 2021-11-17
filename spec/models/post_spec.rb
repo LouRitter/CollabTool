cb0079cd75559b68d1618fa349275d21140b5e7f
Lou Ritter <louritter1@gmail.com>
2021-11-16 23:28:32 -0500

    - Add the bootstrap layout to the home page - Add CSS to make home page layout's stylistic and responsive design changes

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,12 +3,12 @@ require 'rails_helper'
 RSpec.describe Post, type: :model do
   context 'Associations' do
     it 'belongs_to user' do
-      association = described_class.reflect_on_association(:user).macro
+      association = Post.reflect_on_association(:user).macro
       expect(association).to eq :belongs_to
     end
 
     it 'belongs_to category' do
-      association = described_class.reflect_on_association(:category).macro
+      association = Post.reflect_on_association(:category).macro
       expect(association).to eq :belongs_to
     end
   end
